51d2def779495e3b7e670dbf34a8c60dd2b4625a
Kamil Dąbrowski <nameczanin@gmail.com>
2015-06-27 16:45:39 +0200

    Add Constants enum.

---
+++ core/src/net/hiddendungeons/enums/Constants.java
@@ -0,0 +1,9 @@
+package net.hiddendungeons.enums;
+
+public interface Constants {
+	public interface Player {
+		public static final float MaxSpeed = 10f;
+		public static final float Acceleration = 100000f;
+		public static final float Friction = 2f;
+	}
+}
